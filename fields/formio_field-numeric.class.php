e2ab8385ddb3cb7ec9f7f7c8cbb91f0a54ffe893
pospi <pospi@spadgos.com>
2011-05-24 20:46:20 +1000

    fixed numeric validation to allow floating point numbers and negatives
    added additional validators to Numeric field for range & integer constraints

--- fields/formio_field-numeric.class.php
+++ fields/formio_field-numeric.class.php
@@ -7,14 +7,19 @@ class FormIOField_Numeric extends FormIOField_Text
 {
 	// override the regex validator's error message for this field type
 	public static $VALIDATOR_ERRORS = array(
-		'regexValidator' => "Must contain numbers only"
+		'regexValidator' => "Must be a number",
+		'greaterThanValidator' => "Must be greater than \$2",
+		'lessThanValidator' => "Must be less than \$2",
+		'minValueValidator' => "Must be at least \$2",
+		'maxValueValidator' => "Must not be over \$2",
+		'integerValidator' => "Must be a whole number",
 	);
 
 	// validate with regex validator internally
 	protected $validators = array(
 		array(
 			'func' => 'regexValidator',
-			'params' => array('/^\d*$/')
+			'params' => array('/^(-?\d*\.?\d*)?$/')
 		)
 	);
 
@@ -24,6 +29,26 @@ class FormIOField_Numeric extends FormIOField_Text
 		$vars = parent::getBuilderVars();
 		$vars['behaviour'] = 'numeric';
 		return $vars;
+	}
+
+	final protected function greaterThanValidator($expected) {
+		return $this->value > $expected;
+	}
+
+	final protected function lessThanValidator($expected) {
+		return $this->value < $expected;
+	}
+
+	final protected function minValueValidator($expected) {
+		return $this->value >= $expected;
+	}
+
+	final protected function maxValueValidator($expected) {
+		return $this->value <= $expected;
+	}
+
+	final protected function integerValidator() {
+		return intval($this->value) == $this->value;
 	}
 }
 ?>
