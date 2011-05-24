c4363a856cbf3b545a7c4d59e1ab33e7868fbe84
pospi <pospi@spadgos.com>
2011-05-24 20:39:20 +1000

    fixed up button and reset field includes for new inheritance hierarchy

--- fields/formio_field-button.class.php
+++ fields/formio_field-button.class.php
@@ -3,8 +3,6 @@
  *
  */
 
-require_once(FORMIO_FIELDS . 'formio_field-reset.class.php');
-
 class FormIOField_Button extends FormIOField_Text
 {
 	public $buildString = '<div class="row{$alt? alt}{$classes? $classes}"><label>{$desc}</label><input type="button" name="{$name}" id="{$id}"{$value? value="$value"}{$js? onclick="$js"} /><p class="hint">{$hint}</p></div>';
--- fields/formio_field-reset.class.php
+++ fields/formio_field-reset.class.php
@@ -3,6 +3,8 @@
  * Reset extends from Button since we don't want its value included in form data output
  */
 
+require_once(FORMIO_FIELDS . 'formio_field-button.class.php');
+
 class FormIOField_Reset extends FormIOField_Button
 {
 	public $buildString = '<input type="reset" name="{$name}" id="{$id}"{$value? value="$value"}{$classes? class="$classes"} />';
