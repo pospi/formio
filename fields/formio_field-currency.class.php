575ab8b3ed86aa0c9c654cc6a5aef4cd0c8a9f77
pospi <pospi@spadgos.com>
2011-05-26 22:01:46 +1000

    made currency extend from numeric, for use of validators

--- fields/formio_field-currency.class.php
+++ fields/formio_field-currency.class.php
@@ -3,7 +3,9 @@
  *
  */
 
-class FormIOField_Currency extends FormIOField_Text
+require_once(FORMIO_FIELDS . 'formio_field-numeric.class.php');
+
+class FormIOField_Currency extends FormIOField_Numeric
 {
 	public $buildString = '<div class="row{$alt? alt}{$classes? $classes}"><label for="{$id}">{$desc}{$required? <span class="required">*</span>}</label><span class="currency"><span>$</span><input type="text" name="{$name}" id="{$id}"{$value? value="$value"}{$maxlen? maxlength="$maxlen"}{$behaviour? data-fio-type="$behaviour"}{$validation? data-fio-validation="$validation"}{$dependencies? data-fio-depends="$dependencies"} /></span>{$error?<p class="err">$error</p>}<p class="hint">{$hint}</p></div>';
 
