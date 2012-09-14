d2bc9484ebf4ee7782f6aa8b6b31c12df258ace7
pospi <pospi@spadgos.com>
2012-09-14 21:43:51 +1000

    give fieldsets the row class so as to be targetable by dependencies

--- fields/formio_field-fieldsetstart.class.php
+++ fields/formio_field-fieldsetstart.class.php
@@ -5,7 +5,7 @@
 
 class FormIOField_Fieldsetstart extends FormIOField_Raw
 {
-	public $buildString = '<fieldset id="{$id}"><legend>{$desc}</legend>';
+	public $buildString = '<fieldset class="row" id="{$id}"><legend>{$desc}</legend>';
 
 	public function getHTML(&$spinVar)
 	{
