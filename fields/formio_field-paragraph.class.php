09561e7c781ecee234bae54514725eca454077b1
pospi <pospi@spadgos.com>
2012-09-17 19:27:03 +1000

    added inline styling of paragraph elements and output alt class if being striped

--- fields/formio_field-paragraph.class.php
+++ fields/formio_field-paragraph.class.php
@@ -5,6 +5,6 @@
 
 class FormIOField_Paragraph extends FormIOField_Raw
 {
-	public $buildString = '<p id="{$id}"{$classes? class="$classes"}>{$desc}</p>';
+	public $buildString = '<p id="{$id}" class="{$classes}{$alt? alt}"{$styles? style="$styles"}>{$desc}</p>';
 }
 ?>
