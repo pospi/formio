e090e9453708da14ca4ade4f160f29aecbdf2679
pospi <pospi@spadgos.com>
2013-04-26 08:20:46 +0100

    actually, credit card input needs dropdown code

--- fields/formio_field-creditcard.class.php
+++ fields/formio_field-creditcard.class.php
@@ -3,7 +3,9 @@
  * Credit card input field. Contains separate inputs for card information.
  */
 
-class FormIOField_Creditcard extends FormIOField_Multiple
+require_once(FORMIO_FIELDS . 'formio_field-dropdown.class.php');
+
+class FormIOField_Creditcard extends FormIOField_Dropdown
 {
 	public $buildString = '<div class="row blck credit{$alt? alt}{$classes? $classes}" id="{$id}"{$dependencies? data-fio-depends="$dependencies"}{$validation? data-fio-validation="$validation"}>
 		<label for="{$id}_type">{$desc}{$required? <span class="required">*</span>}</label>
