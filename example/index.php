5a0d66d2dac7b65fafed99bd0734ddba49d66f1e
pospi <pospi@spadgos.com>
2014-02-06 19:04:48 +1000

    dont include jquery twice when on site

--- example/index.php
+++ example/index.php
@@ -228,7 +228,9 @@ if (!defined('DID_HEADER')) :
 	}
 ?>
 </div>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<?php if (!(defined('DID_HEADER') && class_exists('P_Site'))) : ?>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<?php endif; ?>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
 <script type="text/javascript" src="<?php echo $localPath; ?>lib/jquery-tokeninput/src/jquery.tokeninput.js"></script>
 <script type="text/javascript" src="<?php echo $localPath; ?>formio.js"></script>
